012586fe544a0dff46385f19a7c2ff8eeebaa56d
JuanDaniel00 <juanuribe@colegioguanenta.edu.co>
2022-07-12 10:15:15 -0500

    Desarrollo: segundo video

---
+++ video2_php.php
@@ -0,0 +1,19 @@
+<?php 
+    $nombre = "Juan - ";
+    echo $nombre;
+
+    $color = 'azul';
+    $COLOR = 'verde - ';
+    echo $COLOR;
+
+    define("codigo", 123456  );
+    echo codigo;
+
+    $n = 5;
+    $nn = 2.6;
+    $nnn = true;
+    echo $nnn;
+    echo gettype($n);
+
+    var_dump($nn);
+?>
